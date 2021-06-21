515445c4af673805d49a1b547897f1b9072d21fd
jumbo pere <jumboperebara0@gmail.com>
2021-06-21 14:29:40 +0100

    m

--- src/container.js
+++ src/container.js
@@ -5,10 +5,10 @@ import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 import Home from './home';
 import About from './about';
-import Products from './Products'
+//import Products from './Products'
 import Contact from './contact';
 import Pictures from './pictures'
-import Productx from "./Products/Products"
+//import Productx from "./Products/Products"
 import Productsx from './Products/Products';
 
 
